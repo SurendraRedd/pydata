8beb7ed65732fd2a456b230ada90b3739a2c8ed8
Surendra Reddy <surendraelectronics@gmail.com>
2023-03-05 23:10:23 +0000

    initial development

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 import streamlit as st
+import pandas as pd
+from pydataset import data
 
-st.title('🎈 App Name')
+st.title('🎈 pydataset')
 
-st.write('streamlit basic code structure!')
+selected_data = st.sidebar.selectbox('Select a dataset', data().dataset_id)
+title_data = data()[ data()['dataset_id'] == selected_data]['title']
+
+st.header('Datasets')
+st.subheader('List of dataset')
+with st.expander('Show list of dataset'):
+    st.write(data())
+
+st.subheader(f'Selected data (`{selected_data}`)')
+st.info(title_data)
+st.write(data(selected_data))
